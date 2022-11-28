cebab4fe4fde9741c5f2d1e806c5c637032228b3
Ilia <polikarpovilja@mail.ru>
2022-11-28 22:55:56 +0300

    new c

---
+++ Dz29/Program.cs
@@ -0,0 +1,35 @@
+﻿int length = UserNumber("Введите количество элементов массива: "); 
+int[] userArray = new int[length];
+FillArray(length);
+PrintArray(userArray);
+
+void FillArray(int length)
+{
+        for(int i = 0; i<length; i++)
+    {
+         userArray[i]= UserNumber($"\nВведите элемент массива {i}: ");
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write($"]");
+}
+
+static int UserNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
