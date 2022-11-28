1f2d2df9bc0b5972321a71850318596183a77976
Ilia <polikarpovilja@mail.ru>
2022-11-28 23:04:45 +0300

    more

---
+++ dz25/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int numA = GetNumFromUser("Введите целое число A: ","Ошибка ввода!");
+int numB = GetNumFromUser("Введите целое число B: ","Ошибка ввода!");
+int result = AvB(numA, numB);
+Console.WriteLine($"{numA}^{numB} -> {result}");
+
+int GetNumFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    } 
+}
+
+int AvB(int number1, int number2)
+{
+    int sum= 1;
+    
+    for (int i = 0; i < number2; i ++) sum *= number1;
+
+    return sum;
+}
+    
---
+++ dz27/Program.cs
@@ -0,0 +1,40 @@
+﻿/// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+Console.Clear();
+
+int number = UserInput("Введите число: ", "Ошибка ввода!");
+Console.WriteLine($"{number} -> {count(number)}");
+
+int UserInput(string message, string errorMessage)
+{    
+     while(true)
+    {
+        Console.Write(message);
+        if( int.TryParse(Console.ReadLine(), out int userNumber))
+
+            return userNumber;
+        Console.WriteLine(errorMessage);
+     } 
+}
+
+int count(int number)
+{
+    int result = 0;
+    if(number<0)
+    {
+    number=number*(-1);
+    while(number > 0)
+    {
+     result+=number%10;
+     number /=10;
+    }
+  return result*(-1);
+    }
+   else if(number>0)
+    while(number > 0)
+     {
+      result+=number%10;
+      number /=10;
+     }
+      return result;
+}
